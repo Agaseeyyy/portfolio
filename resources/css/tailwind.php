e11eb5d4a8872b707e7499f0a000e4ecfd04dece
Agassi <agaseeyyy@agaseeyyy>
2025-08-29 22:11:57 +0800

    chore: setup folders and db

---
+++ resources/css/tailwind.php
@@ -0,0 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+<style type="text/tailwindcss">
+      @theme {
+        --color-clifford: #da373d;
+      }
+    </style>
