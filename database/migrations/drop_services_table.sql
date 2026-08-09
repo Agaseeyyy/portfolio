4fdd8aa8ea0e0f02aa2658abae425a1a08fb212e
Agaseeyyy <bustargaagassi@gmail.com>
2026-08-10 01:11:00 +0800

    feat: add drop_services_table.sql migration script matching project naming conventions
    Simplify database migration to a single clean drop_services_table.sql script matching the naming pattern of add_project_details.sql.

---
+++ database/migrations/drop_services_table.sql
@@ -0,0 +1,4 @@
+-- Remove services_tbl from existing database
+-- Run this SQL on phpMyAdmin if you already have the database created
+
+DROP TABLE IF EXISTS `services_tbl`;
--- database/migrations/drop_services_tbl.sql
+++
@@ -1,4 +0,0 @@
--- Migration Script for InfinityFree phpMyAdmin
--- Removes unused services_tbl table from the database
-
-DROP TABLE IF EXISTS `services_tbl`;
